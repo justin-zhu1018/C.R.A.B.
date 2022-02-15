1c7fd84c770e9dd5c14f2d2bff578426bd23c306
Justin Zhu <justin.zhu1018@gmail.com>
2022-02-15 14:41:01 -0800

    Merge + Confirmation Message Handling

--- index.js
+++ index.js
@@ -47,70 +47,197 @@ app.message('sleep', async ({ message, say}) => {
     await say(`Sleep now <@${message.user}>!`);
 });
 
-app.message('!reviewers', async ({message, say}) => {
-    const membersRes = await app.client.conversations.members({
+app.message('members', async ({message, say}) => {
+    const members_res = await app.client.conversations.members({
         channel: message.channel
     });
-    console.log(message.text);
-    console.log(membersRes);
+    console.log(members_res);
+    const members = members_res.members;
+    members.splice(members.indexOf(message.user), 1);
+    await say(`Members are: `);
+    for (elem of members_res.members) {
+        await say(`<@${elem}>`);
+    }
+});
 
-    const regex = /\d+$/;
-    try {
-      const messageMatch = message.text.match(regex)[0];
-      console.log(message.text.match(regex));
-
-      const membersNum = parseInt(messageMatch);
-      console.log(membersNum);
-      if (membersNum < 1) {
-        await say('Please input a number greater than 0');
-        return;
-      }
-
-      const members = membersRes.members;
-
-      if (membersNum > members.length) {
-        await say('Number of reviewers to select is higher than the members count');
-        return;
-      }
-
-      members.splice(members.indexOf(message.user), 1);
-      await say(`Random members: `);
-      for (let i=0; i<membersNum; i++) {
-        const rand = Math.floor(Math.random()*members.length);
-        await say(`<@${members[rand]}>`);
-        members.splice(rand, 1);
-      }
-
-      await say({
-        blocks: [
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": `Do you wish to reroll <@${message.user}>?`
-            },
-            "accessory": {
-              "type": "button",
+app.message('Confirm', async({message, client, logger}) => {
+  try {
+      const {channel, ts} = message;
+      const confirmText = `Please confirm that these members are available for code review. Otherwise, reroll another combination! <@${message.user}>`
+      const result = await client.chat.postEphemeral({
+          channel,
+          blocks: [
+            {
+              "type": "section",
               "text": {
-                "type": "plain_text",
-                "text": "Reroll"
-              },
-              "action_id": "reroll_click"
+                "type": "mrkdwn",
+                "text": confirmText,
+              }
+            },
+            {
+              "type": "section",
+              "fields": [
+                {
+                  "type": "mrkdwn",
+                  "text": "*Sample:*\nField"
+                },
+              ],
+            },
+            {
+              "type": "actions",
+              "elements": [
+                {
+                  "type": "button",
+                  "text": {
+                    "type": "plain_text",
+                    "emoji": true,
+                    "text": "Approve"
+                  },
+                  "style": "primary",
+                  "value": channel+':'+message.user,
+                  "action_id": "confirm_approve"
+                },
+                {
+                  "type": "button",
+                  "text": {
+                    "type": "plain_text",
+                    "emoji": true,
+                    "text": "Deny"
+                  },
+                  "style": "danger",
+                  "value": channel+':'+message.user,
+                  "action_id": "confirm_deny"
+                },
+                {
+                  "type": "button",
+                  "text": {
+                    "type": "plain_text",
+                    "emoji": true,
+                    "text": "Reroll 🎲"
+                  },
+                  "value": channel+':'+message.user,
+                  "action_id": "confirm_reroll"
+                },
+              ]
             }
-          }
-        ],
-        text: `Do you wish to reroll <@${message.user}>?`
-    });
+          ],
+          user: message.user
+      });
+      logger.info(result);
   }
-  catch (err) {
-    say('Please input number of reviewers to select');
+  catch (error) {
+      logger.error(error);
   }
 });
 
-app.action('reroll_click', async ({ body, ack, say }) => {
+app.action('confirm_approve', async ({ body, ack, say, client}) => {
+  // Acknowledge the action
+  try {
+    await ack();
+    // console.log(body.actions[0]);
+    const params = body.actions[0].value.split(':');
+    // console.log(params);
+    await client.chat.postEphemeral({
+      channel: params[0],
+      user: params[1],
+      text: `Approve!`
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  // await say(`Approved <@${body.user.id}>!`);
+});
+
+app.action('confirm_deny', async ({ body, ack, say, client}) => {
   // Acknowledge the action
   await ack();
-  await say(`<@${body.user.id}> rerolled!`);
+  const params = body.actions[0].value.split(':');
+  // console.log(params);
+  await client.chat.postEphemeral({
+    channel: params[0],
+    user: params[1],
+    text: `Deny!`
+  });
+  // await say(`Deny <@${body.user.id}>!`);
+});
+
+app.action('confirm_reroll', async ({ body, ack, say, client }) => {
+  // Acknowledge the action
+  await ack();
+  const params = body.actions[0].value.split(':');
+  // console.log(params);
+  await client.chat.postEphemeral({
+    channel: params[0],
+    user: params[1],
+    text: `Reroll!`
+  });
+  // await say(`Reroll <@${body.user.id}>!`);
+});
+
+app.message('!reviewers', async ({message, say}) => {
+  const membersRes = await app.client.conversations.members({
+      channel: message.channel
+  });
+  console.log(message.text);
+  console.log(membersRes);
+
+  const regex = /\d+$/;
+  try {
+    const messageMatch = message.text.match(regex)[0];
+    console.log(message.text.match(regex));
+
+    const membersNum = parseInt(messageMatch);
+    console.log(membersNum);
+    if (membersNum < 1) {
+      await say('Please input a number greater than 0');
+      return;
+    }
+
+    const members = membersRes.members;
+
+    if (membersNum > members.length) {
+      await say('Number of reviewers to select is higher than the members count');
+      return;
+    }
+
+    members.splice(members.indexOf(message.user), 1);
+    await say(`Random members: `);
+    for (let i=0; i<membersNum; i++) {
+      const rand = Math.floor(Math.random()*members.length);
+      await say(`<@${members[rand]}>`);
+      members.splice(rand, 1);
+    }
+
+    await say({
+      blocks: [
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": `Do you wish to reroll <@${message.user}>?`
+          },
+          "accessory": {
+            "type": "button",
+            "text": {
+              "type": "plain_text",
+              "text": "Reroll"
+            },
+            "action_id": "reroll_click"
+          }
+        }
+      ],
+      text: `Do you wish to reroll <@${message.user}>?`
+  });
+}
+catch (err) {
+  say('Please input number of reviewers to select');
+}
+});
+
+app.action('reroll_click', async ({ body, ack, say }) => {
+// Acknowledge the action
+await ack();
+await say(`<@${body.user.id}> rerolled!`);
 });
 
 (async () => {
